b0b185527696ae77842c72a4e542527ee45dde20
Pavel Shitikov <pashicop@gmail.com>
2022-03-16 15:58:09 +0300

    Task 1 complete

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+def get_ingridients(i):
+    ingr_list = []
+    for ingr in range(int(list[i + 1].rstrip("\n"))):
+        ingr_list.append(list[i + 2 + ingr].rstrip("\n"))
+        # print(ingr_list)
+    return ingr_list
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def get_ingr(list):
+    new_list_ingr = []
+    for ingr in list:
+        ingr_dict = {}
+        ingr_part = ingr.split("|")
+        ingr_dict['ingridient_name'] = ingr_part[0]
+        ingr_dict['quantity'] = int(ingr_part[1])
+        ingr_dict['measure'] = ingr_part[2]
+        new_list_ingr.append(ingr_dict)
+    return new_list_ingr
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    cook_book = {}
+    with open("1.txt", encoding='utf-8' ) as file:
+        list = file.readlines()
+        # print(list)
+        next_recipe = 0
+        while next_recipe != "END":
+            # get_ingr(get_ingridients(next_recipe))
+            cook_book[list[next_recipe].rstrip("\n")] = get_ingr(get_ingridients(next_recipe))
+            print(cook_book)
+            try:
+                next_recipe = list.index("\n", next_recipe) + 1
+            except ValueError:
+                next_recipe = "END"
+                # print("END")
+            # next_recipe = list.index("\n",next_recipe)
+            print(next_recipe)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
